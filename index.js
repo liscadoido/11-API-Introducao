c3c1ad8bee8a0e7ba6f83e4305de5267ee09e839
liscadoido <132668870+liscadoido@users.noreply.github.com>
2023-08-04 23:55:13 +0000

    att 12

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//index.js
+import { selectUsuarios } from "./bd.js";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+dotenv.config();
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
-
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
     nome: "Thallys Henriques Vilela",      // Substitua pelo seu nome
