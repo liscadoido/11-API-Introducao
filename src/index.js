29ecc4d8fb2b4431b8a901ee6448401174399f68
liscadoido <132668870+liscadoido@users.noreply.github.com>
2023-08-31 19:04:34 +0000

    14

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "API para CRUD usuario: link_gitgub",
+    message: "API para CRUD usuario: https://github.com/liscadoido",
   });
 });
 
